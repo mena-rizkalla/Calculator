721f7f587a56d1541913bd70c9b402ed8680d85d
mena <mena.rizkalla1@gmail.com>
2024-03-06 08:52:15 +0200

    implement delete and enter operation

--- app/src/main/java/com/example/calculator/CalculatorViewModel.kt
+++ app/src/main/java/com/example/calculator/CalculatorViewModel.kt
@@ -10,5 +10,41 @@ class CalculatorViewModel: ViewModel() {
     var state by mutableStateOf(CalculatorState())
         private set
 
+    fun onAction(action: CalculatorActions){
+        when(action){
+            is CalculatorActions.Number -> enterNumber(action.number)
+            is CalculatorActions.Decimal -> enterDecimal()
+            is CalculatorActions.Clear -> state = CalculatorState()
+            is CalculatorActions.Operation -> enterOperation(action.operation)
+            is CalculatorActions.Calculate -> perfomCalculation()
+            is CalculatorActions.Delete -> delete()
+        }
+    }
+
+    private fun enterNumber(number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    private fun delete() {
+       when{
+           state.number2.isNotBlank() -> state = state.copy(number2 = state.number2.dropLast(1))
+           state.operation != null -> state = state.copy(operation = null)
+           state.number1.isNotBlank() -> state = state.copy(number1 = state.number1.dropLast(1))
+       }
+    }
+
+    private fun enterOperation(operation: CalculatorOperation) {
+        if (state.number1.isNotBlank()){
+            state = state.copy(operation = operation)
+        }
+    }
+
+    private fun enterDecimal() {
+        TODO("Not yet implemented")
+    }
+
+    private fun perfomCalculation() {
+        TODO("Not yet implemented")
+    }
 
 }
