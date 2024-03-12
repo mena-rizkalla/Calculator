b6b57ffa3e230db04939ffeef2209b8a7e9722cc
mena <mena.rizkalla1@gmail.com>
2024-03-12 09:23:45 +0200

    implement database class

---
+++ app/src/main/java/com/example/calculator/CalculatorDatabase.kt
@@ -0,0 +1,25 @@
+package com.example.calculator
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Item::class] , version = 1 , exportSchema = false)
+abstract class CalculatorDatabase : RoomDatabase() {
+    abstract fun CalculatorDao(): CalculatorDao
+
+    companion object {
+        @Volatile
+        private var Instance: CalculatorDatabase? = null
+
+        fun getDatabase(context: Context): CalculatorDatabase {
+            // if the Instance is not null, return it, otherwise create a new database instance.
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, CalculatorDatabase::class.java, "calculator_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
